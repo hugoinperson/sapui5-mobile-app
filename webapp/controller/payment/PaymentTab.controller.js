1d760ad6e98312247528386b89aecadc22115991
Jerry Hsu <hsuyachieh@gmail.com>
2017-07-08 18:59:52 -0500

    finish payment tab

--- webapp/controller/payment/PaymentTab.controller.js
+++ webapp/controller/payment/PaymentTab.controller.js
@@ -10,9 +10,18 @@ sap.ui.define(
 		var Controller = BaseController.extend('ui5.mobile.app.controller.payment.PaymentTab');
 		
 		Controller.prototype.onInit = function () {
+			this._oPaymentCarousel = this.getView().byId('ui5Mobile-paymentTab-carousel');
 		};
 
 		Controller.prototype.onBeforeRendering = function () {
+		};
+
+		Controller.prototype.onPaymentPrev = function () {
+			this._oPaymentCarousel.previous();
+		};
+
+		Controller.prototype.onPaymentNext = function () {
+			this._oPaymentCarousel.next();
 		};
 
 		return Controller;
